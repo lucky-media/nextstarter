144c08a742149ed3cef0ae7787bd2dd4eb5ebabe
Lokman Musliu <lokman.musliu@gmail.com>
2021-04-07 18:55:47 +0200

    Updated nextjs, updated dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,7 +30,7 @@ module.exports = {
         md: '768px',
         lg: '1024px',
         xl: '1280px',
-        '2xl': '1536px',
+        '2xl': '1280px',
       },
     }),
     // eslint-disable-next-line no-undef
